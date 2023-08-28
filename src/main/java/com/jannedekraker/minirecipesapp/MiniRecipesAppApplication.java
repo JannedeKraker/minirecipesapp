d6947e0f82c6445ca2e8f7cb59b2b25c59f1c051
janne <ennaj_janne@hotmail.com>
2023-08-28 12:28:20 +0200

    Initial commit

---
+++ src/main/java/com/jannedekraker/minirecipesapp/MiniRecipesAppApplication.java
@@ -0,0 +1,13 @@
+package com.jannedekraker.minirecipesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniRecipesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniRecipesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jannedekraker/minirecipesapp/MiniRecipesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jannedekraker.minirecipesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniRecipesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
