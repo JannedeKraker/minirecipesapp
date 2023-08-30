c2ee3a2c621261ee6c94395d65fc4ddbfbddb519
janne <ennaj_janne@hotmail.com>
2023-08-30 11:32:13 +0200

    try to get the recipes on the front end website

--- frontend/src/components/RecipeList.jsx
+++ frontend/src/components/RecipeList.jsx
@@ -1,29 +1,35 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { useState, useEffect } from "react";
 
-const RecipeList = () => {
-  const [recipes, setRecipes] = useState([]);
+function RecipeList() {
+  const [data, setData] = useState([]);
+
+  async function fetchData() {
+    try {
+      const response = await fetch("http://localhost:8080/api/v1/recipes");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
+    }
+  }
 
   useEffect(() => {
-    axios
-      .get("/recipes") // Hier moet je het juiste backend-eindpunt opgeven
-      .then((response) => setRecipes(response.data))
-      .catch((error) => console.error(error));
+    fetchData();
   }, []);
 
   return (
-    <div className="bg-gray-100 min-h-screen p-8">
-      <h1 className="text-2xl font-semibold mb-4">All Recipes</h1>
-      <div className="grid grid-cols-2 gap-4">
-        {recipes.map((recipe) => (
-          <div key={recipe.id} className="bg-white p-4 rounded shadow-md">
-            <h2 className="text-lg font-semibold mb-2">{recipe.title}</h2>
-            <p className="text-gray-700">{recipe.description}</p>
-          </div>
+    <div>
+      <h1>Data List</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
         ))}
-      </div>
+      </ul>
     </div>
   );
-};
+}
 
 export default RecipeList;
